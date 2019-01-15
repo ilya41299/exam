7c09da93828ac81f223a2e8eb82856e6279b793a
ilya41299 <32190925+ilya41299@users.noreply.github.com>
2019-01-15 23:11:05 +0300

    Create lock_guard.cpp

---
+++ lock_guard.cpp
@@ -0,0 +1,29 @@
+/*adopt_lock_t это грубо говоря флаг который указывает lock_guard`у,
+что передаваемый mutex уже захвачен и не нужно вызывать вызыватьт метод lock,
+в противном случае будет повторный захват mutex`а и неопределенное поведение*/
+
+template <typename T>
+class lock_guard
+{
+    T& mutex;
+
+public:
+    explicit lock_guard(T& mut)
+        : mutex(mut)
+    {
+        mutex.lock();
+    }
+
+    lock_guard(T& mut, adopt_lock_t)
+        : mutex(mut)
+    {
+    }
+
+    ~lock_guard()
+    {
+        mutex.unlock();
+    }
+
+    lock_guard(const lock_guard&) = delete;
+    lock_guard& operator=(const lock_guard&) = delete;
+};
