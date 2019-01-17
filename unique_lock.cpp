ec3608e6793e85d69bc21945932fe4f594863a96
ilya41299 <32190925+ilya41299@users.noreply.github.com>
2019-01-17 15:45:42 +0300

    Update unique_lock.cpp

--- unique_lock.cpp
+++ unique_lock.cpp
@@ -1,53 +1,64 @@
-
+#include <mutex>
 template <class Mutex>
 class unique_lock
 {
-	Mutex &m_;
-	bool is_lock = false;
+	Mutex *m_;
+	bool is_lock;
 public:
-	unique_lock() {}
+	unique_lock() 
+	{
+		m_ = nullptr;
+		is_lock = false;
+	}
 
 	~unique_lock()
 	{
 		if (is_lock) 
 		{
-			m_.unlock();
+			(*m_).unlock();
 			is_lock = false;
 		}
 	}
 
 	unique_lock(unique_lock&& other) noexcept : m_(other.m_)
 	{
-		//m_ = other.m_;
+		other.m_ = nullptr;
+		is_lock = other.is_lock;
+		other.is_lock = false;
 	}
 
-	explicit unique_lock(Mutex & m) : m_(m)
+	explicit unique_lock(Mutex & m) 
 	{
-		m_.lock();
+		m_ = &m;
+		(*m_).lock();
 		is_lock = true;
 	}
-	unique_lock(Mutex& m, std::defer_lock_t t) noexcept :m_(m) {}
-
-	unique_lock(Mutex& m, std::adopt_lock_t t) :m_(m) 
+	unique_lock(Mutex& m, std::defer_lock_t t) noexcept 
 	{
+		m_ = &m;
+	}
+
+	unique_lock(Mutex& m, std::adopt_lock_t t) 
+	{
+		m_ = &m;
 		is_lock = true;
 	}
 
 	void lock()
 	{
-		m_.lock();
+		(*m_).lock();
 		is_lock = true;
 	}
 
 	void unlock()
 	{
-		m_.unlock();
+		(*m_).unlock();
 		is_lock = false;
 	}
 
 	unique_lock<Mutex>& operator=(unique_lock<Mutex>&& other) 
 	{
-		m_=other.m_;
+		(*m_)=other.m_;
 		is_lock = other.is_lock;
 	}
 };
@@ -57,8 +68,8 @@ int main()
 {
 	std::mutex m;
 	std::defer_lock_t t;
-	unique_lock<std::mutex> l1(m, t);
-	l1.lock();
+	unique_lock<std::mutex> l1(m);
+	
 	unique_lock<std::mutex> l2(std::move(l1));
 
 	return 0;
